01228d61a59df060404c54dceb94f599d3133422
Christian Mercado <n01419184@unf.edu>
2019-10-27 23:36:24 -0400

    Added Leaderboard Model and View

---
+++ UNFHackathonManagementSystem/Areas/LeaderBoard/Controllers/LeaderboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UNFHackathonManagementSystem.Models;
+namespace UNFHackathonManagementSystem.Areas.LeaderBoard.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
