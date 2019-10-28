8a2c8e268738ee33208ad2f9c489371d36d8abb0
Christian Mercado <n01419184@unf.edu>
2019-10-27 22:29:28 -0400

    test

--- UNFHackathonManagementSystem/Program.cs
+++ UNFHackathonManagementSystem/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace UNFHackathonManagementSystem
 {
-    public class Program
+    public class Program //testifchriscangetongithub 
     {
         public static void Main(string[] args)
         {
