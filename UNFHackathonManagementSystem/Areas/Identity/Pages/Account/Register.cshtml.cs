d2ad2c698e5ac7ec21f5f86de6d1d1908eae7c73
Dustin White <n01397879@ospreys.unf.edu>
2019-11-20 16:13:18 -0500

    Added Username and Major fields to registration form

--- UNFHackathonManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UNFHackathonManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -56,6 +56,17 @@ namespace UNFHackathonManagementSystem.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Text)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Major")]
+            public string Major { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
