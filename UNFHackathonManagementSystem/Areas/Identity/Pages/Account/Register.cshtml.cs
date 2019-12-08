539a9409cfabcdd04993d63cd248e182b644c05d
Michael Zulfekar <n00929403@ospreys.unf.edu>
2019-12-08 01:08:47 -0500

    Update Join Competion

--- UNFHackathonManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UNFHackathonManagementSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -57,6 +57,9 @@ namespace UNFHackathonManagementSystem.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
+
+
+
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Text)]
