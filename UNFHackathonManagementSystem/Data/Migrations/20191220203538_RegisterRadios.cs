5cc29253fedd768d5a540e2af008421999bc171f
Dustin White <n01397879@ospreys.unf.edu>
2019-12-20 19:55:36 -0500

    Added radio buttons and input for major to registration form

---
+++ UNFHackathonManagementSystem/Data/Migrations/20191220203538_RegisterRadios.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UNFHackathonManagementSystem.Data.Migrations
+{
+    public partial class RegisterRadios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
